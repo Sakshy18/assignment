8bc9f4f18f81f10a0e16050bdc39372e577360db
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2022-08-30 16:00:14 +0530

    Update trial.sql

--- DBMS/PLSQL/trial.sql
+++ DBMS/PLSQL/trial.sql
@@ -2,6 +2,7 @@ BEGIN
 dbms_output.put_line('hellow world');
 END;
 /
-SQL> set serveroutput on
+SQL> set serveroutput on //edit filename.sql has to be used for creating file
+
 SQL>@trial.sql
 hellow world
