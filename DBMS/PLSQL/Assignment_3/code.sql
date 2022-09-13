32abcd39b041b3f5b7286dbeae61ba51d02cb006
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2022-09-13 15:54:33 +0530

    Create code.sql

---
+++ DBMS/PLSQL/Assignment_3/code.sql
@@ -0,0 +1,15 @@
+create table Borrower(Roll_no int,Name varchar(25),DateOfIssue date,NameOfBook varchar(25), Status varchar(25));
+insert into Borrower values(40,'Sakshi Thombre','12/09/2022','Data networks','Issued')
+insert into Borrower values(03,'Kalp Patel','12/18/2022','TOC','Issued')
+insert into Borrower values(43,'Gauri Waiker','12/03/2022','CNS','Issued')
+insert into Borrower values(30,'Om Gawande','12/11/2022','TOC','Issued')
+insert into Borrower values(19,'Swapnil Bonde','12/12/2022','DSA','Issued')
+insert into Borrower values(23,'Yash Lomate','12/18/2022','SPOS','Issued')
+insert into Borrower values(20,'Tejas Chaudhari','12/15/2022','HTML,CSS','Issued')
+insert into Borrower values(48,'Sujay Shraunik','12/31/2022','DBMS','Issued')
+insert into Borrower values(53,'Shreya Kulkarni','12/01/2022','TOC','Issued')
+insert into Borrower values(51,'Akshat Thawait','12/07/2022','SPM','Issued')
+insert into Borrower values(10,'Sweta Singh','12/27/2022','DSA','Issued')
+insert into Borrower values(62,'Ashu Gupta','12/13/2022','SPOS','Issued')
+SELECT * FROM Borrower
+
